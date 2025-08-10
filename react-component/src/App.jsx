99ff02c54bb0aee96c5eec182382db5867fa7e01
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-10 20:19:31 +0530

    understanding react component

---
+++ react-component/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PaymentButton from './PaymentButton'
+const App = () => {
+  return (
+    <PaymentButton />    
+  )
+}
+
+export default App
---
+++ react-component/src/PaymentButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PaymentButton = () => {
+  return (
+    <>
+          <h2>Make the payement!</h2>
+          <h3>and learn lifetime.</h3>
+          <h4><b>Price: $20.00</b></h4>
+
+          <button>Checkout</button>
+    </>
+  )
+}
+
+export default PaymentButton
