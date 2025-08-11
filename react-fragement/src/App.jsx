aba4a88ab26bc0cee4f7a3c86daa52f4ab48cdd2
Ripan Baidya <116362939+baidya-ripan-024@users.noreply.github.com>
2025-08-11 21:22:54 +0530

    react fragement

---
+++ react-fragement/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Backend Frameworks</h1>
+
+      <ul>
+        <li>Node.js</li>
+        <li>Django</li>
+        <li>Spring Boot</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
